d7393504563ce1fb0aa25d7cc5af36ed0eababcf
JustinZZW <zhiwei92@163.com>
2018-09-12 10:57:27 +0800

    add modes in readMSP

--- R/tools.R
+++ R/tools.R
@@ -2,23 +2,36 @@
 #' @description read MSP spectra files
 #' @author Zhiwei Zhou
 #' \email{zhouzw@@sioc.ac.cn}
+#' @param file the file name
+#' @param mode standard: extract name, mz and RT from the file, which fit for MSP data exported from QI software; all: extract all information in the file
 #' @return
 #' A list object. info: MS1 information, including ms1, rt etc.; spec: the MS/MS spectrum
 
-setGeneric('readMSP', function(file) {
+setGeneric('readMSP', function(file,
+                               mode=c('standard', 'all')) {
+  devtools::use_package('dplyr')
+
   msp.data.list <- ListDB(file)
   nr.num.pk <- grep('Num Peaks', stringr::str_to_title(msp.data.list[[1]]))
   info.spec <- lapply(msp.data.list, function(msp.data) {
     info.list <- strsplit(msp.data[1:nr.num.pk], split = ': ')
     info <- do.call(cbind, info.list)
     colnames(info) <- info[1, ]
-    mz <- round(as.numeric(info[2,3]), digits = 4)
-    rt <- round(as.numeric(strsplit(info[2, "Comment"], split = "_")[[1]][1])*60, digits = 0)
-    name <- info[2, "Comment"]
-    # info <- matrix(c(mz, rt), ncol = 2)
-    info <- data.frame(mz=mz, rt=rt)
-    rownames(info) <- name
-    # colnames(info) <- c("mz", "rt")
+
+    if (mode=='standard') {
+      mz <- round(as.numeric(info[2,3]), digits = 4)
+      rt <- round(as.numeric(strsplit(info[2, "Comment"], split = "_")[[1]][1])*60, digits = 0)
+      name <- info[2, "Comment"]
+      # info <- matrix(c(mz, rt), ncol = 2)
+      info <- data.frame(mz=mz, rt=rt)
+      rownames(info) <- name
+      # colnames(info) <- c("mz", "rt")
+    } else {
+      info <- as.data.frame(tibble::as.tibble(info))
+      info <- info[-1,,drop=F]
+      rownames(info) <- NULL
+    }
+
     spec.list <- strsplit(msp.data[(nr.num.pk+1):length(msp.data)], ' |\\t')
     spec.list <- lapply(spec.list, as.numeric)
     spec <- do.call(rbind, spec.list)[, c(1, 2), drop = FALSE]
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ImmsTools
+
+You can install ImmsTools from GitHub.
+
+# install.packages("devtools")
+devtools::install_github("tidyverse/dplyr")
